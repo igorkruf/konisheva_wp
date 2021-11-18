4aa219738eca8485a0a6943396bd34e595dd340f
root <root@89-108-64-98.cloudvps.regruhosting.ru>
2021-11-18 12:03:14 +0500

    Готовый продакшн

--- wp-content/themes/mystomat/index.php
+++ wp-content/themes/mystomat/index.php
@@ -1,2 +1,35 @@
 <?php
+$root = get_template_directory_uri();
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="icon" href="<?php echo $root; ?>/favicon.ico">
+    <title>Моя стоматология</title>
+    <link href="<?php echo $root; ?>/css/app.0f444592.css" rel="preload" as="style">
+    <link href="<?php echo $root; ?>/css/chunk-vendors.68b021be.css" rel="preload" as="style">
+    <link href="<?php echo $root; ?>/js/app.fc475d91.js" rel="preload" as="script">
+    <link href="<?php echo $root; ?>/js/chunk-vendors.a20ca841.js" rel="preload" as="script">
+    <link href="<?php echo $root; ?>/css/chunk-vendors.68b021be.css" rel="stylesheet">
+    <link href="<?php echo $root; ?>/css/app.0f444592.css" rel="stylesheet">
+</head>
+
+<body>
+    <noscript>
+        <strong>We're sorry but client doesn't work properly without JavaScript enabled.
+            Please enable it to continue.</strong>
+    </noscript>
+    <div id="app"></div>
+    <!-- built files will be auto injected -->
+    <script type="text/javascript" src="<?php echo $root; ?>/js/chunk-vendors.a20ca841.js"></script>
+    <script type="text/javascript" src="<?php echo $root; ?>/js/app.fc475d91.js"></script>
+</body>
+
+</html>
